1846f3b8e7bb3b5d38dd234c9861c20b948018ab
sujimatts <40743779+sujimatts@users.noreply.github.com>
2023-09-26 14:21:18 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,5 @@
 def call(String appName, String environment) {
     script {
-        echo "Deploying ${appName} to ${environment environment"
-        org.mycompany.utils.Myutilities.printHello()
+        echo "Deploying ${appName} to ${environment} environment"
     }
 }
