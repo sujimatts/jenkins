c4d38d401049a3dcdb6fd384ddd01d9a8d84accb
sujimatts <sujimatts@suj.com>
2023-09-26 16:47:13 +0530

    renamed to call as prefix

--- vars/multiple.groovy
+++ vars/multiple.groovy
@@ -1,6 +1,9 @@
 // shared_library/vars/multi.groovy
 
-def call() {
-    echo "Cricket function called"
+def myFunction1() {
+    echo "Function 1 called"
 }
 
+def myFunction2() {
+    echo "Function 2 called"
+}
